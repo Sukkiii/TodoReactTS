274a328b90eb56d099031bc77b274faa0e194fbe
sukki <knk951213@naver.com>
2024-01-04 19:07:27 +0900

    🌱feat: project setting

---
+++ todoreactts/.eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
---
+++ todoreactts/src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import { Box } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Box>왜이래</Box>
+    </>
+  )
+}
+
+export default App
