dae500c1b0e15309eb5cfa703b96b08b0bd3c707
Sukki <knk951213@naver.com>
2024-01-12 17:41:39 +0900

    💄feat: calendar make start

--- src/components/common/Calendar.tsx
+++ src/components/common/Calendar.tsx
@@ -1,3 +1,23 @@
+import { Box, Typography } from '@mui/material'
+import dayjs from 'dayjs'
+
+const days = ['일', '월', '화', '수', '목', '금', '토']
+
 export default function Calendar() {
-  return <div></div>
+  const today: dayjs.Dayjs = dayjs()
+
+  return (
+    <Box className='flex flex-col w-full mt-4 gap-middle'>
+      <Box>
+        <Typography className='text-size-sub text-main-hover-color'>
+          {today.format('YYYY년 MM월')}
+        </Typography>
+      </Box>
+      <Box className='flex justify-around'>
+        {days.map((_) => (
+          <Box key={_}>{_}</Box>
+        ))}
+      </Box>
+    </Box>
+  )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,20 +1,3 @@
-export interface Image {
-  id: number
-  fileName: string
-  uploadPath: string
-  size: number
-  createdAt: Date
-  updatedAt: Date
-  deletedAt?: Date
-}
-
-export interface Category {
-  id: number
-  name: string
-  flag?: number
-  createdAt?: Date
-  updatedAt?: Date
-  deletedAt?: Date
-  parentId?: Category
-  subCategories?: Category[]
+export interface CalendarType {
+  today: Date
 }
