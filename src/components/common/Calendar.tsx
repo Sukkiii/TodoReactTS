a35e3dbee72acd78fd07a351defcf1aff9d574a9
sukki <knk951213@naver.com>
2024-01-16 22:35:01 +0900

    ⭐️feat: calendar make 2

--- src/components/common/Calendar.tsx
+++ src/components/common/Calendar.tsx
@@ -1,22 +1,84 @@
+import { useState } from 'react'
 import { Box, Typography } from '@mui/material'
+import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded'
+import KeyboardArrowLeftRoundedIcon from '@mui/icons-material/KeyboardArrowLeftRounded'
 import dayjs from 'dayjs'
 
 const days = ['일', '월', '화', '수', '목', '금', '토']
 
 export default function Calendar() {
-  const today: dayjs.Dayjs = dayjs()
+  const [currentMonth, setCurrentMonth] = useState(dayjs())
+
+  const startOfMonth = currentMonth.startOf('month')
+  const endOfMonth = currentMonth.endOf('month')
+  const daysInMonth = endOfMonth.date()
+  const startDayOfWeek = startOfMonth.day()
+
+  const handlePreMonth = () => {
+    setCurrentMonth(currentMonth.subtract(1, 'month'))
+  }
+
+  const handleNextMonth = () => {
+    setCurrentMonth(currentMonth.add(1, 'month'))
+  }
+
+  const generateCalendarDays = () => {
+    const daysArray = []
+    let currentDay = 1
+
+    for (let i = 0; i < 6; i++) {
+      const week = []
+
+      for (let j = 0; j < 7; j++) {
+        if ((i === 0 && j < startDayOfWeek) || currentDay > daysInMonth) {
+          week.push(<Box key={`empty-${j}`} className='calendar-day'></Box>)
+        } else {
+          week.push(
+            <Box key={currentDay} className='calendar-day text-sub-hover-color'>
+              {currentDay}
+            </Box>,
+          )
+          currentDay++
+        }
+      }
+
+      daysArray.push(
+        <Box
+          key={`week-${i}`}
+          className='grid grid-cols-7 auto-rows-auto justify-items-center text-sub-hover-color'
+        >
+          {week}
+        </Box>,
+      )
+    }
+
+    return daysArray
+  }
 
   return (
     <Box className='flex flex-col w-full mt-4 gap-middle'>
-      <Box>
-        <Typography className='text-size-sub text-main-hover-color'>
-          {today.format('YYYY년 MM월')}
+      <Box className='grid grid-cols-7 justify-items-center'>
+        <Typography className='col-span-3 text-size-sub text-main-hover-color'>
+          {currentMonth.format('YYYY년 MM월')}
         </Typography>
+        <KeyboardArrowLeftRoundedIcon
+          onClick={handlePreMonth}
+          className='col-span-1 col-end-7 cursor-pointer fill-main-color'
+        />
+        <KeyboardArrowRightRoundedIcon
+          onClick={handleNextMonth}
+          className='col-span-1 col-end-8 cursor-pointer fill-main-color'
+        />
       </Box>
-      <Box className='flex justify-around'>
+      <Box className='grid grid-cols-7 justify-items-center'>
         {days.map((_) => (
-          <Box key={_}>{_}</Box>
+          <Box key={_} className='text-sub-hover-color'>
+            {_}
+          </Box>
         ))}
+      </Box>
+      <Box className='flex flex-col flex-wrap gap-big'>
+        {generateCalendarDays()}
       </Box>
     </Box>
   )
--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,27 +1,40 @@
 import { Box, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 import HomeIcon from '@mui/icons-material/Home'
 import TodayIcon from '@mui/icons-material/Today'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import dayjs from 'dayjs'
 
 export default function Footer() {
+  const today = dayjs().format('YYYYMMDD')
+
   return (
     <Box className='flex justify-between w-full h-20 border-t-2 border-t-gray-200'>
-      <Box className='flex flex-col flex-wrap items-center content-center justify-center w-1/3 border-r-2 border-r-gray-200'>
+      <Link
+        to='/'
+        className='flex flex-col flex-wrap items-center content-center justify-center w-1/3 border-r-2 cursor-pointer border-r-gray-200'
+      >
         <HomeIcon className='fill-main-hover-color' />
         <Typography className='text-main-hover-color text-size-text'>
           Home
         </Typography>
-      </Box>
-      <Box className='flex flex-col flex-wrap items-center content-center justify-center w-1/3 border-r-2 border-r-gray-200'>
+      </Link>
+      <Link
+        to={`/today/${today}`}
+        className='flex flex-col flex-wrap items-center content-center justify-center w-1/3 border-r-2 cursor-pointer border-r-gray-200'
+      >
         <TodayIcon className='fill-sub-color' />
         <Typography className='text-sub-color text-size-text'>Today</Typography>
-      </Box>
-      <Box className='flex flex-col flex-wrap items-center content-center justify-center w-1/3 '>
+      </Link>
+      <Link
+        to='/mypage'
+        className='flex flex-col flex-wrap items-center content-center justify-center w-1/3 cursor-pointer '
+      >
         <AccountCircleIcon className='fill-sub-color' />
         <Typography className='text-sub-color text-size-text'>
           MyPage
         </Typography>
-      </Box>
+      </Link>
     </Box>
   )
 }
