ac21c4de52da87a11f26c55db4419721b37251c7
sukki <knk951213@naver.com>
2024-01-26 21:34:40 +0900

    🚧 feat: clover image resetting

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,3 @@
 import Logo from '@/assets/fightingRabbit.png'
-import CloverDefault from '@/assets/cloverDefault.svg'
-import Clover1 from '@/assets/clover1.svg'
-import Clover2 from '@/assets/clover2.svg'
-import Clover3 from '@/assets/clover3.svg'
-import Clover4 from '@/assets/clover4.svg'
 
-export { Logo, CloverDefault, Clover1, Clover2, Clover3, Clover4 }
+export { Logo }
--- src/components/schedule/Clovers.tsx
+++ src/components/schedule/Clovers.tsx
@@ -1,42 +1,52 @@
-import { CloverDefault } from '@/assets'
 import { useState } from 'react'
-import { Clover1 } from '@/assets'
 import { Box } from '@mui/material'
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded'
 
-export default function Clovers() {
-  const [isDone, setIsDone] = useState(false)
+interface CloversProps {
+  colors: string[]
+}
+export default function Clovers({ colors }: CloversProps) {
+  let fill = [colors[0], colors[0], colors[0], colors[0]]
+  const [, setIsDone] = useState(false)
 
   const handleDone = () => {
     setIsDone(true)
-    if (isDone) {
-      setIsDone(false)
-    }
   }
+
+  switch (colors.length) {
+    case 1:
+      fill = [colors[0], colors[0], colors[0], colors[0]]
+      break
+    case 2:
+      fill = [colors[0], colors[0], colors[1], colors[1]]
+      break
+    case 3:
+      fill = [colors[0], colors[0], colors[1], colors[2]]
+      break
+    case 4:
+      fill = [colors[0], colors[1], colors[2], colors[3]]
+      break
+    default:
+      return ['sub', 'sub', 'sub', 'sub']
+  }
+
   return (
-    <Box className='w-4 h-4 mr-6 cursor-pointer' onClick={handleDone}>
-      {isDone ? (
-        <>
-          <Box className='flex'>
-            <img src={Clover1}></img>
-            <img className='rotate-90' src={Clover1}></img>
-          </Box>
-          <Box className='flex'>
-            <img className='rotate-[270deg]' src={Clover1}></img>
-            <img className='rotate-180' src={Clover1}></img>
-          </Box>
-        </>
-      ) : (
-        <>
-          <Box className='flex'>
-            <img src={CloverDefault}></img>
-            <img className='rotate-90' src={CloverDefault}></img>
-          </Box>
-          <Box className='flex'>
-            <img className='rotate-[270deg]' src={CloverDefault}></img>
-            <img className='rotate-180' src={CloverDefault}></img>
-          </Box>
-        </>
-      )}
+    <Box className='w-6 mr-3 cursor-pointer' onClick={handleDone}>
+      <Box className='relative h-8 ml-2 rotate-45'>
+        <FavoriteRoundedIcon
+          fill={fill[1]}
+          className={`absolute top-[-3.5px] w-5 h-5 fill-clover-${fill[1]}`}
+        />
+        <FavoriteRoundedIcon
+          className={`absolute top-[4px] w-5 h-5 rotate-90 left-2 fill-clover-${fill[2]}`}
+        />
+        <FavoriteRoundedIcon
+          className={`absolute top-[4px] left-[-8px] rotate-[270deg] w-5 h-5 fill-clover-${fill[0]}`}
+        />
+        <FavoriteRoundedIcon
+          className={`absolute w-5 h-5 rotate-180 top-[12px] fill-clover-${fill[3]}`}
+        />
+      </Box>
     </Box>
   )
 }
