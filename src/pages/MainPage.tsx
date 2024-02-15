3a08374919ec9de99bf7beda5efd424afe9a8ac7
sukki <knk951213@naver.com>
2024-02-15 17:48:29 +0900

    🚧 feat: page scroll to top add 2

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
 import { Calendar } from '@/components/common'
 import { Schedules } from '@/components/schedule'
+import { ScrollToTop } from '@/components/layouts'
 
 const MainPage = () => {
   return (
     <>
+      <ScrollToTop />
       <Calendar />
       <Schedules />
     </>
